ca5f2809d765df811db7702e0c8429dccf583c7b
Sushant-Coder-01 <sushantmangore45@gmail.com>
2024-10-04 00:07:14 +0530

    Ep. 02 - Created Our Own React App

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
-/*
-<div id = "parent">
-    <div id = "child1">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-    <div id = "child2">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-</div>
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", {id: "parent"}, [ 
     React.createElement("div",{id: "child1"}, [ 
         React.createElement("h1",{},"I'm h1 tag!"),
         React.createElement("h2",{},"I'm h2 tag!"),
     ]),
-    React.createElement("div",{id: "child1"}, [
+    React.createElement("div",{id: "child2"}, [
         React.createElement("h1",{},"I'm h1 tag!"),
         React.createElement("h2",{},"I'm h2 tag!"),
     ]),
