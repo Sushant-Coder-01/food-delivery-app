2e70166846f338b928b616d488ac451868af14d8
Sushant-Coder-01 <sushantmangore45@gmail.com>
2024-10-08 18:57:30 +0530

    Learned about useEffect() Hook and Fetch Live Data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,28 @@
 import ResturantCart from "./ResturantCards";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  const [listOfResturants, setListOfResturants] = useState(resList);
+  const [listOfResturants, setListOfResturants] = useState([]);
 
+  useEffect(() => {
+    fethData();
+  }, []);
+
+  const fethData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    console.log(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+
+    setListOfResturants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
   return (
     <div className="body">
       <div className="filter">
