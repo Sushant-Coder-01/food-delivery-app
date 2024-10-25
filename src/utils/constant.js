4cd530250f91bf32401da1e389f44040e1d1635d
Sushant-Coder-01 <sushantmangore45@gmail.com>
2024-10-25 10:02:24 +0530

    add proxy

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -12,7 +12,7 @@ export const CART_LOGO =
   "https://www.rawshorts.com/freeicons/wp-content/uploads/2017/01/orange_shoppictbasket_1484336514.png";
 
 export const REST_MENU =
-  "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.61610&lng=73.72860&restaurantId=";
+  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.61610&lng=73.72860&restaurantId=";
 
 export const REST_ITEMS_IMAGES =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2024/7/7/";
